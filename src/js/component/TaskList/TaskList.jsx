7f6bb3beb79e7a095407257d2beec07641c93251
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-09-09 13:37:32 +0000

    Dependiendo de la prioridad de la tarea tendrá un icono u otro. También agregué un botón para tener la elección de enseñar o no el estado de las tareas (hecha o no)

--- src/js/component/TaskList/TaskList.jsx
+++ src/js/component/TaskList/TaskList.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./tasklist.css";
 import BotonSwitch from "../BotonSwitch";
 
 const TaskList = ({ list, numbersOfItems }) => {
+  const [showSwitches, setShowSwitches] = useState(false);
+
+  const toggleSwitches = () => {
+    setShowSwitches(!showSwitches);
+  };
+
+  const getIconForTaskId = (taskId) => {
+    switch (taskId) {
+      case "high":
+        return <i className="fa-solid fa-triangle-exclamation"></i>;
+      case "medium":
+        return <i class="fa-solid fa-hand"></i>;
+      case "low":
+        return <i class="fa-solid fa-hourglass-start"></i>;
+      default:
+        return null;
+    }
+  };
   return (
     <div className="toDo">
       <div className="toDoTitle">TO DO LIST</div>
@@ -11,12 +29,15 @@ const TaskList = ({ list, numbersOfItems }) => {
         <ul className="fa-ul">
           {list.map((task, index) => (
             <li className="text" key={index}>
-              {task.label}{" "}
-              <BotonSwitch id={`cb${index}`} />
+              {getIconForTaskId(task.id)} {task.label}
+              {showSwitches && <BotonSwitch id={`cb${index}`} />}
             </li>
           ))}
         </ul>
       </div>
+      <button className="switchButton" onClick={toggleSwitches}>
+        {showSwitches ? "Hide task status" : "Show task status"}
+      </button>
     </div>
   );
 };
